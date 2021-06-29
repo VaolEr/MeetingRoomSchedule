a53f44f3e7d41a54d30c2f01f6e3c997119cfe0b
VaolEr <eremizin@gmail.com>
2021-06-29 19:55:40 +0300

    Initial commit

---
+++ src/main/java/ru/VaolEr/meetingroomschedule/MeetingRoomScheduleApplication.java
@@ -0,0 +1,13 @@
+package ru.VaolEr.meetingroomschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/VaolEr/meetingroomschedule/MeetingRoomScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.VaolEr.meetingroomschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
